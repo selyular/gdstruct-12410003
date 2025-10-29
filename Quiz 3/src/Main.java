af11bdadbda98e41db096e1f5887a8d93e5d1569
sel <ysabelagatha.andes@benilde.edu.ph>
2025-10-29 15:18:37 +0800

    Answer for Quiz 3

--- Quiz 3/src/Main.java
+++ Quiz 3/src/Main.java
@@ -14,24 +14,26 @@ public class Main {
         stack.push(new Player(6, "MegurineLuka", 98));
         stack.push(new Player(7, "Gumi", 99));
 
+        stack.printStack();
+
         Scanner scanner = new Scanner(System.in);
         Random rand = new Random();
 
         ArrayStack queue = new ArrayStack(20); // players currently queued
         int totalGames = 0;
 
-        System.out.println("\n=== Matchmaking Simulation (Players Requeue Automatically) ===");
-        System.out.println("Double-click Enter to print turn...");
+        System.out.println("\n=== Matchmaking Simulation ===");
+        System.out.println("Double-click Enter to process turns...");
 
         while (totalGames < 10) {
-            scanner.nextLine(); // simulate pressing enter
+            scanner.nextLine(); // pressing enter
 
-            // random no. joining d queue
+            // random no. of players joining queue
             int joining = rand.nextInt(7) + 1;
             System.out.println("\nTurn started: " + joining + " player(s) attempting to join...");
 
+            // Move random players from the main stack into the queue
             for (int i = 0; i < joining; i++) {
-                // if stack is empty, recycle players who finished
                 if (stack.isEmpty()) {
                     refillFromCompleted(stack);
                 }
@@ -46,7 +48,7 @@ public class Main {
             System.out.println("\nCurrent queue:");
             queue.printStack();
 
-            // starts matches if there are 5 or more players
+            // starts if there are 5 or more players
             while (queue.size() >= 5 && totalGames < 10) {
                 System.out.println("\n--- Game " + (totalGames + 1) + " Started ---");
                 ArrayStack finished = new ArrayStack(5);
@@ -59,6 +61,7 @@ public class Main {
 
                 totalGames++;
 
+                // all players go back into the main pool
                 System.out.println("Game ended! Players return to the pool.\n");
                 while (!finished.isEmpty()) {
                     stack.push(finished.pop());
